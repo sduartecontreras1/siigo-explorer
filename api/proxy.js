45b81a16fcccd1cbdbc38228f1819350b7c3571c
sduartecontreras1 <sduartecontreras1@gmail.com>
2026-03-23 17:08:59 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   const headers = {
   "Content-Type": "application/json",
-  "Partner-Id": "siigoapi"
+  "Partner-Id": "JASAsesores"
 };
   if (req.headers.authorization) headers["Authorization"] = req.headers.authorization;
 
